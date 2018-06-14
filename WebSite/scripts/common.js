6e7825ad7c5197405d095477830b4a3e6f54ecde
Alexander Zakharov <Alexander.Zakharov@harman.com>
2018-06-14 16:58:41 +0300

    Added contry code
    Change-Id: Ie11fd98bac0ab23b1c8852fd47e67141432cd23c

--- WebSite/scripts/common.js
+++ WebSite/scripts/common.js
@@ -25,14 +25,40 @@ $(function(){
             createCustomdSelect();
         });
 
-        $("#accountPhone").mask("+9 (999) 999-9999");
+        $("#accountPhone").on('keydown', function (e) {
+            // Allow: backspace, delete, tab, escape, enter and .
+            if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
+                 // Allow: Ctrl/cmd+A
+                (e.keyCode == 65 && (e.ctrlKey === true || e.metaKey === true)) ||
+                 // Allow: Ctrl/cmd+C
+                (e.keyCode == 67 && (e.ctrlKey === true || e.metaKey === true)) ||
+                 // Allow: Ctrl/cmd+X
+                (e.keyCode == 88 && (e.ctrlKey === true || e.metaKey === true)) ||
+                 // Allow: home, end, left, right
+                (e.keyCode >= 35 && e.keyCode <= 39)) {
+                     // let it happen, don't do anything
+                     return;
+            }
+            // Ensure that it is a number and stop the keypress
+            if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+                e.preventDefault();
+            }
+        });
+
+        $("#accountPhone").intlTelInput({
+            autoPlaceholder: 'aggressive',
+            separateDialCode: true,
+            autoHideDialCode: false,
+            onlyCountries: ['uk', 'ru', 'us', 'in'],
+            utilsScript: "masked-input/js/utils.js",
+            formatOnDisplay: true
+        });
     });
 });
 
 
 const isPhoneValid = (phone) => {
-    const phoneRegex = /^(\+\d)?\d{10}$/;
-    return phoneRegex.test(phone)
+    return $("#accountPhone").intlTelInput("isValidNumber");
 }
 
 const getName = () => {
@@ -40,7 +66,7 @@ const getName = () => {
 }
 
 const getPhone = () => {
-    return $("#accountPhone").val();
+    return $("#accountPhone").intlTelInput("getNumber");
 }
 
 const getReasonId = () => {
@@ -50,17 +76,17 @@ const getReasonId = () => {
 const signup = () => {
     const name = getName();
     if(!name) {
-        alert("please enter Name");
+        alert("Please enter Name");
         return;
     }
     const phone = '+' + getPhone().replace(/\D/gi, ''); // regex return only numbers after masking
     if(!isPhoneValid(phone)) {
-        alert("please enter valid phone (10 digits)");
+        alert("Please enter valid phone");
         return;
     }
     const reasonId = getReasonId();
     if(!reasonId) {
-        alert("please select the Reason");
+        alert("Please select the Reason");
         return;
     }
 
